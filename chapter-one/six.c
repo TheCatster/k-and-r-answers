64ea9ce74224ead1a1044adea5afa33cc846dd55
Daniil Rose <daniil.rose@member.fsf.org>
2022-01-01 17:33:32 -0500

    Fix one-six and add solution for one-seven

--- chapter-one/six.c
+++ chapter-one/six.c
@@ -6,9 +6,9 @@ int main() {
   // From my understanding, this verifies if ~getchar() != EOF~ is 0 or 1.
   c = (getchar() != EOF);
   if ((c == 0) || (c == 1)) {
-    printf("Definitely 0 or 1.");
+    printf("Definitely 0 or 1.\n");
   } else {
-    printf("Uh oh.");
+    printf("Uh oh.\n");
   }
 
   while ((c = getchar()) != EOF) {
