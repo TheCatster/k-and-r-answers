fcee562885ad7b5a4bd85646fc074c820c56eac0
Daniil Rose <daniil.rose@member.fsf.org>
2021-12-31 19:37:54 -0500

    Add CMake build system and solution for one-two

--- chapter-one/main.c
+++
@@ -1,6 +0,0 @@
-#include "chapter-one.h"
-
-int main() {
-  printf("hello, world\a\n");
-  return 0;
-}
--- chapter-one/src/main.c
+++
@@ -1,7 +0,0 @@
-#include "chapter-one.h"
-#include <stdio.h>
-
-int main() {
-  printf("hello, world\a\n");
-  return 0;
-}
